945bdd7b124b94aa0a3a2e8ca9e129f11f90eb3f
alessandraciccone <alessandraciccone@icloud.com>
2025-10-17 12:14:16 +0200

    Initial commit

---
+++ src/test/java/alessandraciccone/Gestione_Viaggi_Aziendali/GestioneViaggiAziendaliApplicationTests.java
@@ -0,0 +1,13 @@
+package alessandraciccone.Gestione_Viaggi_Aziendali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneViaggiAziendaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
